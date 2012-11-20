528b6b13408d429e799ffd3e3e20b72bb2125f14
Jack Gassett <jack.gassett@gadgetfactory.net>
2012-11-19 17:24:45 -0700

    Fix for SID chip drum not working and 18 second delay when starting.
    Turned out 1Mhz clock was not working well. Changed it to a DCM and all
    is good now.

--- zpu/hdl/zpuino/boards/papilio-pro/S6LX9/clkgen.vhd
+++ zpu/hdl/zpuino/boards/papilio-pro/S6LX9/clkgen.vhd
@@ -56,7 +56,9 @@ end entity clkgen;
 architecture behave of clkgen is
 
 signal dcmlocked: std_ulogic;
+signal dcmlocked_1mhz: std_logic;
 signal dcmclock: std_ulogic;
+signal dcmclock_1mhz: std_logic;
 
 signal rst1_q: std_logic := '1';
 signal rst2_q: std_logic := '1';
@@ -67,18 +69,23 @@ signal clk0: std_ulogic;
 signal clk1: std_ulogic;
 signal clk2: std_ulogic;
 signal clkin_i_2: std_logic;
-signal clk_div: std_logic;
-signal count: integer;
+-- signal clk_div: std_logic;
+-- signal count: integer;
+
+signal clkin_i_1mhz: std_logic;
+signal clkfb_1mhz: std_logic;
+signal clk0_1mhz: std_logic;
 
 begin
 
   clkout <= clkout_i;
 
   rstout <= rst1_q;
+  
 
-  process(dcmlocked, clkout_i, rstin)
+  process(dcmlocked, dcmlocked_1mhz, clkout_i, rstin)
   begin
-    if dcmlocked='0' or rstin='1' then
+    if dcmlocked='0' or dcmlocked_1mhz='0' or rstin='1' then
       rst1_q <= '1';
       rst2_q <= '1';
     else
@@ -112,20 +119,6 @@ begin
   clk1_inst: BUFG port map ( I => clk1, O => clkout1 );
   clk2_inst: BUFG port map ( I => clk2, O => clkout2 );
   
-process (clk2, rstin) begin
-	if (rstin = '1') then
-		count <= 0;
-	elsif (rising_edge(clk2)) then
-		if (count=95) then
-			clk_div <= not clk_div;
-			count <= 0;
-		else
-			count <= count +1;
-		end if;
-	end if;
-end process;
-clk_1Mhz_out <= clk_div;  
-
 pll_base_inst : PLL_ADV
   generic map
    (BANDWIDTH            => "OPTIMIZED",
@@ -170,5 +163,51 @@ pll_base_inst : PLL_ADV
       REL => '0'
 
    );
+   
+DCM_inst_1mhz : DCM
+  generic map (
+    CLKDV_DIVIDE => 16.0, -- Divide by: 1.5,2.0,2.5,3.0,3.5,4.0,4.5,5.0,5.5,6.0,6.5,7.0,7.5,8.0,9.0,10.0,11.0,12.0,13.0,14.0,15.0 or 16.0
+    CLKFX_DIVIDE => 1,--8, -- Can be any integer from 1 to 32
+    CLKFX_MULTIPLY => 3,--23, -- Can be any integer from 1 to 32
+    CLKIN_DIVIDE_BY_2 => TRUE, -- TRUE/FALSE to enable CLKIN divide by two feature
+    CLKIN_PERIOD => 31.25, -- Specify period of input clock
+    CLKOUT_PHASE_SHIFT => "NONE", -- Specify phase shift of NONE, FIXED or VARIABLE
+    CLK_FEEDBACK => "NONE", -- Specify clock feedback of NONE, 1X or 2X
+    DESKEW_ADJUST => "SYSTEM_SYNCHRONOUS", -- SOURCE_SYNCHRONOUS, SYSTEM_SYNCHRONOUS or an integer from 0 to 15
+    DFS_FREQUENCY_MODE => "LOW", -- HIGH or LOW frequency mode for frequency synthesis
+    DLL_FREQUENCY_MODE => "LOW", -- HIGH or LOW frequency mode for DLL
+    DUTY_CYCLE_CORRECTION => TRUE, -- Duty cycle correction, TRUE or FALSE
+    FACTORY_JF => X"C080", -- FACTORY JF Values
+    PHASE_SHIFT => 0, -- Amount of fixed phase shift from -255 to 255
+    STARTUP_WAIT => FALSE -- Delay configuration DONE until DCM LOCK, TRUE/FALSE
+    )
+  port map (
+    CLK0 => clk0_1mhz, -- 0 degree DCM CLK ouptput
+    CLK180 => open, -- 180 degree DCM CLK output
+    CLK270 => open, -- 270 degree DCM CLK output
+    CLK2X => open, -- 2X DCM CLK output
+    CLK2X180 => open, -- 2X, 180 degree DCM CLK out
+    CLK90 => open, -- 90 degree DCM CLK output
+    CLKDV => dcmclock_1mhz, -- Divided DCM CLK out (CLKDV_DIVIDE)
+    CLKFX => open, -- DCM CLK synthesis out (M/D)
+    CLKFX180 => open, -- 180 degree CLK synthesis out
+    LOCKED => dcmlocked_1mhz, -- DCM LOCK status output
+    PSDONE => open, -- Dynamic phase adjust done output
+    STATUS => open, -- 8-bit DCM status bits output
+    CLKFB => clkfb_1mhz, -- DCM clock feedback
+    CLKIN => clkin_i, -- Clock input (from IBUFG, BUFG or DCM)
+    PSCLK => '0', -- Dynamic phase adjust clock input
+    PSEN => '0', -- Dynamic phase adjust enable input
+    PSINCDEC => '0', -- Dynamic phase adjust increment/decrement
+    RST => '0' -- DCM asynchronous reset input
+  );
+
+  clkfx_inst_1mhz: BUFG
+    port map (
+      I => dcmclock_1mhz,
+      O => clk_1Mhz_out
+    );
+	
+	clkin_i_1mhz <= clkout_i;	   
 
 end behave;
